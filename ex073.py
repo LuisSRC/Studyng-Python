81bbc02fe05a0b683c5afe75d9c7cfd324842777
LuisSRC <98289175+LuisSRC@users.noreply.github.com>
2022-04-04 18:17:36 -0300

    Delete ex073.py

--- ex073.py
+++
@@ -1,9 +0,0 @@
-times = ('Atlético - MG', 'Flamengo', 'Palmeiras', 'Fortaleza',
-          'Corinthias', 'Bragantino', 'Fluminense', 'America - MG',
-          'Atlético - GO', 'Santos', 'Ceara - SC', 'Internacional',
-        'São Paulo', 'Atlético - PR', 'Cuiabá', 'Juventude',
-        'Gremio', 'Bahia', 'Sport Recife', 'Chapecoense')
-print(f'Os 5 primeiros colocados são: {times[:5]}')
-print(f'Os ultimos 4 colocados são: {times[-4:]}')
-print(f'Times em ordem alfabética: {sorted(times)}')
-print(f'O chapencoense está na {times.index("Chapecoense")+1}ª posição.')
