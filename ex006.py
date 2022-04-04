0ec3f077105761c5c726b90cedd72ca102d5f7b2
LuisSRC <98289175+LuisSRC@users.noreply.github.com>
2022-04-04 18:08:55 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input('Digite um numero'))
-d = n1*2
-t = n1*3
-rq = n1**(1/2)
-print('Analisando o numero {}\n O dobro é {}\n O triplo é {}\n E a raiz quadrada é {:.2f}'.format(n1, d, t, rq))
