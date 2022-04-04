60b8ff95b4620b9bbeb1cb1e13a773580c3c381c
LuisSRC <98289175+LuisSRC@users.noreply.github.com>
2022-04-04 18:10:18 -0300

    Delete ex017.py

--- ex017.py
+++
@@ -1,6 +0,0 @@
-from math import hypot
-co = float(input('Comprimento do cateto oposto'))
-ca = float(input('Comprimento do cateto adjacente'))
-hi = hypot(co, ca)
-print('A Hipotenusa vale {}'.format(hi))
-
