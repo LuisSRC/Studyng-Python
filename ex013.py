202b449048567a08639d1c2bfaeaef9e3afce436
LuisSRC <98289175+LuisSRC@users.noreply.github.com>
2022-04-04 18:09:48 -0300

    Delete ex013.py

--- ex013.py
+++
@@ -1,3 +0,0 @@
-salario = float(input('Qual é o salário do funcionario? R$'))
-novo = salario + (salario * 15 /100)
-print('O novo salario do funcionário que ganhava R${:.2f}, com reajuste de 15% passará a ser R${:.2f}'.format (salario, novo))
