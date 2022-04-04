8900ee7e74dbae0d31071a353c7a3ca162354bd4
LuisSRC <98289175+LuisSRC@users.noreply.github.com>
2022-04-04 18:17:00 -0300

    Delete ex067.py

--- ex067.py
+++
@@ -1,7 +0,0 @@
-while True:
-    n = int(input('Quer ver a tabuada de qual valor?: '))
-    if n < 0:
-        print('PROGRAMA TABUADA ENCERRADO, VOLTE SEMPRE')
-        break
-    for c in range(1, 11):
-        print(f'{n} x {c} = {n * c}')
