064d8481a65c90ff85c0c0929baa40742e203639
LuisSRC <98289175+LuisSRC@users.noreply.github.com>
2022-04-04 18:09:42 -0300

    Delete ex012.py

--- ex012.py
+++
@@ -1,8 +0,0 @@
-preço = float(input('Qual é o preço do produto? R$ '))
-novo = preço - (preço * 5 / 100)
-print('O produto que custava R${:.2f} com 5% de desconto vai custar R${:.2f}'.format (preço, novo))
-
-
-
-
-
