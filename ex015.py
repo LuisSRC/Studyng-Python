bdad5e03b707a6beb340323496d6e1119f11e9b7
LuisSRC <98289175+LuisSRC@users.noreply.github.com>
2022-04-04 18:10:02 -0300

    Delete ex015.py

--- ex015.py
+++
@@ -1,6 +0,0 @@
-d = float(input('Digite a quantidade de dias de aluguel: '))
-km = float(input('Digite a quantidade de km rodados: '))
-aluguel = (d * 60) + (0.15 * km)
-print('O total a pagar é de R${:.2f}'.format(aluguel))
-
-
