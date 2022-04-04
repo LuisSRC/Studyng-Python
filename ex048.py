86284cf0abeecf73d63a311ea7a2d3c64d6e1339
LuisSRC <98289175+LuisSRC@users.noreply.github.com>
2022-04-04 18:13:51 -0300

    Delete ex048.py

--- ex048.py
+++
@@ -1,8 +0,0 @@
-soma = 0
-cont = 0
-for c in range(1, 501, 2):
-    if c % 3 == 0:
-        soma += c
-        cont += 1
-print('{} números IMPARES multiplos de 3 com soma no total de {}'.format(cont, soma))
-
